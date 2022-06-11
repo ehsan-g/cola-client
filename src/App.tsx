5e879402bd99a4ce1e854a00a2fc0ea4708504ae
Ehsan G <ehsan@say.company>
2022-06-11 00:38:18 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Pepsi & Coke Business Solution
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://consensys.net/open-roles/3865926/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ConsenSys <code>Full Stack Engineer (Innovation Lab)</code>.
         </a>
       </header>
     </div>
